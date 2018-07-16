9885b99e69adf2d20dfdc59f7e241ddfbd264b15
Daniel Neel <dneelyep@gmail.com>
2018-07-16 19:30:48 -0400

    Add a Packages controller to the API, to begin to handle package searching/etc activity.

---
+++ Server/API/MusicSampleManager.API/Controllers/PackagesController.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MusicSampleManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PackagesController : ControllerBase
+    {
+        public ActionResult<Package> Get([FromQuery] string packageName)
+        {
+            if (packageName == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return new Package(packageName);
+        }
+    }
+}
