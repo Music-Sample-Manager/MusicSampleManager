584878d4d217a48b29406dfdb65a9bf85eea4e98
Daniel Neel <dneelyep@gmail.com>
2018-07-28 13:46:10 -0400

    Add the ability to store revisions of packages in the database.

--- Server/Database/Scripts/Post-Deploy/Script.LoadSeedData.sql
+++ Server/Database/Scripts/Post-Deploy/Script.LoadSeedData.sql
@@ -4,11 +4,29 @@
 USE msmdb
 GO
 
+DECLARE @Landr VARCHAR(MAX) = 'LANDR.SamplePacks',
+		@LiveSchool VARCHAR(MAX) = 'LiveSchool.FreeSamples',
+		@Ableton VARCHAR(MAX) = 'Ableton.FreePacks',
+		@VPO VARCHAR(MAX) = 'VirtualPlayingOrchestra'
+
 INSERT Packages (Identifier)
 VALUES ('LegoWelt.FreeSamples'),
-	   ('LANDR.SamplePacks'),
+	   (@Landr),
 	   ('GroundYourSound'),
-	   ('LiveSchool.FreeSamples'),
-	   ('Ableton.FreePacks'),
-	   ('BoxedEar.FreeKitPacks')
+	   (@LiveSchool),
+	   (@Ableton),
+	   ('BoxedEar.FreeKitPacks'),
+	   (@VPO)
+
+
+INSERT PackageRevisions (PackageId, VersionNumber)
+VALUES ((SELECT Id FROM Packages WHERE Identifier = @Landr), '0.0.0.1'),
+	   ((SELECT Id FROM Packages WHERE Identifier = @Landr), '0.0.0.2'),
+	   ((SELECT Id FROM Packages WHERE Identifier = @Landr), '0.0.1.0'),
+	   ((SELECT Id FROM Packages WHERE Identifier = @Landr), '1.0.0.0'),
+
+	   ((SELECT Id FROM Packages WHERE Identifier = @LiveSchool), '1.0.1.0'),
+	   ((SELECT Id FROM Packages WHERE Identifier = @Ableton), '1.0.1.1'),
+	   ((SELECT Id FROM Packages WHERE Identifier = @Ableton), '1.0.1.1'),
+	   ((SELECT Id FROM Packages WHERE Identifier = @VPO), '3.1.0.0')
 GO
---
+++ Server/Database/Tables/PackageRevisions.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[PackageRevisions]
+(
+    [PackageId] INT NOT NULL, 
+    [VersionNumber] NVARCHAR(20) NOT NULL, 
+    CONSTRAINT [FK_PackageRevisions_ToPackage] FOREIGN KEY (PackageId) REFERENCES Packages(Id)
+)
