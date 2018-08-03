c0e91ce4807880fa8f1a9961e91521367cdb584c
Daniel Neel <dneelyep@gmail.com>
2018-08-02 20:45:22 -0400

    Add a class to represent the notion of a MSM project, which exists on a filesystem. #3

---
+++ Core/Domain.Tests/LocalProjectTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO.Abstractions.TestingHelpers;
+using Xunit;
+
+namespace Domain.Tests
+{
+    public class LocalProjectTests
+    {
+        #region Ctor
+        [Fact]
+        public void LocalProjectCtor_ThrowsArgumentNullException_WhenNullRootFolderIsProvided()
+        {
+            Assert.Throws<ArgumentNullException>(() => new LocalProject(new MockFileSystem(), null));
+        }
+
+        [Fact]
+        public void LocalProjectCtor_ThrowsArgumentNullException_WhenNullFileSystemIsProvided()
+        {
+            Assert.Throws<ArgumentNullException>(() => new LocalProject(null, "SomeFolderHere"));
+        }
+
+        [Fact]
+        public void LocalProjectCtor_ThrowsException_WhenRootFolderDoesNotExist()
+        {
+            Assert.Throws<Exception>(() => new LocalProject(new MockFileSystem(), "SomeRandomFolder"));
+        }
+
+        [Fact]
+        public void LocalProjectCtor_DoesNotThrowException_WhenFileSystemAndRootFolderExists()
+        {
+            var rootFolder = "SomeRootHere";
+            var fileSystem = new MockFileSystem();
+            fileSystem.AddDirectory(rootFolder);
+
+            new LocalProject(new MockFileSystem(), rootFolder);
+        }
+        #endregion
+    }
+}
---
+++ Core/Domain/LocalProject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO.Abstractions;
+
+namespace Domain
+{
+    public class LocalProject
+    {
+        private readonly IFileSystem FileSystem;
+        public readonly string RootFolder;
+
+        public LocalProject(IFileSystem fileSystem, string rootFolder)
+        {
+            if (fileSystem == null)
+            {
+                throw new ArgumentNullException(nameof(fileSystem));
+            }
+
+            if (rootFolder == null)
+            {
+                throw new ArgumentNullException(nameof(rootFolder));
+            }
+
+            if (fileSystem.DirectoryInfo.FromDirectoryName(rootFolder) == null)
+            {
+                throw new Exception($"{rootFolder} does not exist. Invalid project root folder.");
+            }
+
+            RootFolder = rootFolder;
+        }
+    }
+}
