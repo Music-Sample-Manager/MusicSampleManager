03ed0526e27e9cdfd9f8ed7a618f997eb5347f75
Daniel Neel <dneelyep@gmail.com>
2018-08-04 16:59:52 -0400

    Add code to make the API use Entity Framework, and pull the connection string from the right location.

--- Server/API/MusicSampleManager.API/Startup.cs
+++ Server/API/MusicSampleManager.API/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Core.DataLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using PackageDatabase;
 
 namespace MusicSampleManager.API
 {
@@ -26,6 +22,11 @@ namespace MusicSampleManager.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var dbConnString = Configuration["DatabaseConnectionString"];
+
+            services.AddDbContext<DbPackageRepository>(options => options.UseSqlServer(dbConnString));
+            services.AddScoped<IPackageRepository, DbPackageRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
