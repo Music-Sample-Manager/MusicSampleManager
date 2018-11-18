ddda23b5bcfd97fbf2d9dad936e364050a395ac1
Daniel Neel <dneelyep@gmail.com>
2018-11-17 19:43:45 -0500

    Re-arrange package management capability into a more-suitable page.

--- Website/ContributorWebsite/Pages/Index.cshtml.cs
+++ Website/ContributorWebsite/Pages/Index.cshtml.cs
@@ -1,46 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Net.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContributorWebsite.Pages
 {
     public class IndexModel : PageModel
     {
-        [Required, BindProperty, StringLength(128)]
-        public string PackageName { get; set; }
-
-        [Required, BindProperty]
-        public string PackageDescription { get; set; }
-
-        [Required, BindProperty]
-        public int PackageAuthorId { get; set; }
-
-
-
         public void OnGet()
         {
 
-        }
-
-        public async void OnPostAsync()
-        {
-            string baseUrl = "http://localhost:7071"; // https://contributorwebsitebackend.azurewebsites.net
-            string requestUrl = $"{baseUrl}/api/CreatePackage?packageName={PackageName}&packageDescription={PackageDescription}&authorId={PackageAuthorId}";
-
-            using (HttpClient client = new HttpClient())
-            {
-                await client.PostAsync(requestUrl, new StringContent(string.Empty));
-            }
-            
-            //if (!ModelState.IsValid)
-            //{
-            //    return Page();
-            //}
-
-            //_db.Customers.Add(Customer);
-            //await _db.SaveChangesAsync();
-            //return RedirectToPage("/Index");
-        }
+        }        
     }
 }
--- Website/ContributorWebsite/Pages/ManagePackages.cshtml.cs
+++ Website/ContributorWebsite/Pages/ManagePackages.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,9 +11,35 @@ namespace ContributorWebsite.Pages
 {
     public class ManagePackagesModel : PageModel
     {
-        public void OnGet()
-        {
+        [Required, BindProperty, StringLength(128)]
+        public string PackageName { get; set; }
 
+        [Required, BindProperty]
+        public string PackageDescription { get; set; }
+
+        [Required, BindProperty]
+        public int PackageAuthorId { get; set; }
+
+        
+        
+        public async void OnPostAsync()
+        {
+            string baseUrl = "http://localhost:7071"; // https://contributorwebsitebackend.azurewebsites.net
+            string requestUrl = $"{baseUrl}/api/CreatePackage?packageName={PackageName}&packageDescription={PackageDescription}&authorId={PackageAuthorId}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                await client.PostAsync(requestUrl, new StringContent(string.Empty));
+            }
+
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+
+            //_db.Customers.Add(Customer);
+            //await _db.SaveChangesAsync();
+            //return RedirectToPage("/Index");
         }
     }
 }
