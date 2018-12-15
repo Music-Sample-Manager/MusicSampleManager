d3077ffc2436be36d2f76cb4d6a69765ec6fc9e6
Daniel Neel <dneelyep@gmail.com>
2018-12-15 02:07:52 -0500

    Add a stub user profile page.

---
+++ Website/ContributorWebsite/Pages/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PublicWebsite.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
