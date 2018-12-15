039848d7941e0d3ad1c68404b221c0f0e0dc34df
Daniel Neel <dneelyep@gmail.com>
2018-12-15 01:26:34 -0500

    Add a Function to retrieve all package revisions for a given package id.

---
+++ Website/ContributorWebsite.Backend/PackageRevisions/GetPackageRevisionsByPackageId.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using DataLayer;
+using System.Linq;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace ContributorWebsite.Backend.PackageRevisions
+{
+    public static class GetPackageRevisionsByPackageId
+    {
+        [FunctionName(nameof(GetPackageRevisionsByPackageId))]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            int packageId;
+            if (!int.TryParse(req.Query["packageId"], out packageId))
+            {
+                return new BadRequestObjectResult("Please pass a packageId in the query string");
+            }
+
+            using (var dbContext = new MSMDbContext())
+            {
+                var packageRevisions = dbContext.PackageRevisions.Where(pr => pr.PackageId == packageId).ToList();
+
+                return new OkObjectResult(packageRevisions);
+            }
+        }
+    }
+}
