5d6aa7b02b278ac8fbf70501e02e8e77ddcdd98c
Daniel Neel <dneelyep@gmail.com>
2018-12-15 02:09:39 -0500

    List existing packages on a new "Manage Packages" page.

--- Website/ContributorWebsite/Pages/ManagePackages/Index.cshtml.cs
+++ Website/ContributorWebsite/Pages/ManagePackages/Index.cshtml.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Core.DataLayer;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PublicWebsite.APIClient;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ContributorWebsite.Pages
 {
     public class ManagePackagesModel : PageModel
     {
-       
+        private readonly APIClient _apiClient = new APIClient();
+
+        public List<PackageRec> Packages { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            int dummyAuthorId = 1;
+
+            var packages = _apiClient.GetPackagesByAuthorId(dummyAuthorId);
+
+            Packages = await packages;
+        }
     }
 }
