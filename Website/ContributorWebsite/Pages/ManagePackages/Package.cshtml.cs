f141f5117784e016667b8619db2a1838b6de8a5a
Daniel Neel <dneelyep@gmail.com>
2018-12-15 02:17:14 -0500

    Add a package details page to website.

---
+++ Website/ContributorWebsite/Pages/ManagePackages/Package.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Core.DataLayer;
+using DataLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace PublicWebsite.Pages.ManagePackages
+{
+    public class PackageModel : PageModel
+    {
+        private readonly APIClient.APIClient _apiClient = new APIClient.APIClient();
+
+        [BindProperty]
+        public PackageRec Package { get; set; }
+
+        [BindProperty]
+        public List<PackageRevisionRec> PackageRevisions { get; set; }
+
+
+        public void OnGet(int packageId)
+        {
+            Package = _apiClient.GetPackageByIdAsync(packageId).Result;
+            PackageRevisions = _apiClient.GetPackageRevisionsByPackageId(packageId).Result;
+        }
+    }
+}
