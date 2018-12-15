08a50c20fdaa0b92adc91842d9fb126d99581d47
Daniel Neel <dneelyep@gmail.com>
2018-12-15 02:15:49 -0500

    Add Authentication controller with the ability to Sign Out of the application.

---
+++ Website/ContributorWebsite/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PublicWebsite.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignOut()
+        {
+            var callbackUrl = Url.Action(nameof(SignedOut), "Account", values: null, protocol: Request.Scheme);
+            return SignOut(
+                new AuthenticationProperties { RedirectUri = callbackUrl },
+                AzureADDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult SignedOut()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                // Redirect to home page if the user is authenticated.
+                return RedirectToPage("Index");
+            }
+
+            return View();
+        }
+    }
+}
