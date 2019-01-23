5ebfcdda690a4d2cc8afd8d43e2e25a59281f709
Daniel Neel <dneelyep@gmail.com>
2019-01-22 23:46:51 -0500

    Rename unit test project. Fix some tests that were failing in that project.

--- Server/API/API.Tests/PackagesControllerTests.cs
+++ Server/API/API.Tests/PackagesControllerTests.cs
@@ -1,6 +1,7 @@
 using Moq;
-using MusicSampleManager.API.Controllers;
 using PackageDatabase;
+using PublicWebsite;
+using PublicWebsite.APIClient;
 using System;
 using Xunit;
 
@@ -8,11 +9,14 @@ namespace API.Tests
 {
     public class PackagesControllerTests
     {
+        private readonly APIClient client = new APIClient();
+
+
         [Fact]
         public void Get_WithEmptyPackageName_Throws()
         {
             var packageRepo = new Mock<IPackageRepository>();
-            var sut = new PackagesController(packageRepo.Object);
+            var sut = new PackagesController(client, packageRepo.Object);
 
             ArgumentException ex = Assert.Throws<ArgumentException>(() => sut.Get(string.Empty));
 
@@ -23,7 +27,7 @@ namespace API.Tests
         public void Get_WithNullPackageName_Throws()
         {
             var packageRepo = new Mock<IPackageRepository>();
-            var sut = new PackagesController(packageRepo.Object);
+            var sut = new PackagesController(client, packageRepo.Object);
 
             Assert.Throws<ArgumentNullException>(() => sut.Get(null));
             packageRepo.VerifyAll();
