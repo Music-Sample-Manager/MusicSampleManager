a7677ad15bb8e7b5c0e09979937027cb73900a3e
Daniel Neel <dneelyep@gmail.com>
2019-01-23 19:50:14 -0500

    Remove unused using statement.

--- Website/ContributorWebsite.Backend/PackageRevisions/GetPackageRevisionsByPackageId.cs
+++ Website/ContributorWebsite.Backend/PackageRevisions/GetPackageRevisionsByPackageId.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
+
 using DataLayer;
 using System.Linq;
 using Microsoft.Azure.WebJobs.Host;
