290cb8a2aea00b5eb9f266946f1e637d57fb0d8d
Daniel Neel <dneelyep@gmail.com>
2019-05-11 15:18:44 -0400

    Check in PackagesService.API project.

---
+++ Services/PackagesService/PackagesService.API/Controllers/PackagesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PackagesService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PackagesController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult Get(string packageName)
+        {
+            if (packageName == null || packageName == string.Empty)
+            {
+                return new BadRequestObjectResult("packageName cannot be null or empty.");
+            }
+
+            return new OkObjectResult(packageName);
+        }
+    }
+}
