14f43ba0e9f5c60b30a2ffb5be643f86a282b71a
Daniel Neel <dneelyep@gmail.com>
2019-06-02 14:37:48 -0400

    Fix DI setup problems in Functions.

---
+++ Services/PackagesService/PackagesService.API/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using PackagesService.DAL;
+
+[assembly: FunctionsStartup(typeof(PackagesService.API.Startup))]
+namespace PackagesService.API
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddTransient((s) =>
+            {
+                return new MSMDbContext(false);
+            });
+        }
+    }
+}
